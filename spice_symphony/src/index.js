26f27969f6ff519ea273e1f9d7dac67efe125580
Nikhithasriram <nikhithasriram.552@gmail.com>
2024-11-16 23:08:46 +0530

    added reviews page

--- spice_symphony/src/index.js
+++ spice_symphony/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ContactPage from './pages/contact_page';
+import ReviewsPage from './pages/reviws_page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContactPage/>
+    <ReviewsPage/>
   </React.StrictMode>
 );
 
---
+++ spice_symphony/src/pages/reviws_page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../Components/footer'
+import Header from '../Components/header'
+import Reviews from '../Components/reviews'
+function ReviewsPage() {
+    return (
+        <div className=' bg-black text-white'>
+            <Header />
+            <Reviews />
+            <Footer />
+        </div>
+    )
+}
+
+export default ReviewsPage
