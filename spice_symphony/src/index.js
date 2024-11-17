0ed7f854d2a6a7af4ce993b55c0f07e972d7d36e
Nikhitha <87708383+Nikhithasriram@users.noreply.github.com>
2024-11-17 15:45:09 +0530

    Update index.js

--- spice_symphony/src/index.js
+++ spice_symphony/src/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-// import HomePage from './pages/home_page';
-// import Gallery from './pages/gallery_page';
-import GalleryPage from './pages/gallery_page';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GalleryPage/>
-=======
-<<<<<<< HEAD
 import AboutUs from './Components/about_us';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AboutUs/> {/* Use your components here */}
-=======
 import MenuPage from './pages/menu_page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MenuPage/>
->>>>>>> 24f3dd3575a6980bc1baf4506ce4d6be0143c71c
->>>>>>> 2f31dcdde87f2fd4a6b720530ae9b8301bb63bfa
   </React.StrictMode>
 );
 
