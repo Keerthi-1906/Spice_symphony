e6d7b5a791e58555b4805ed5ac450ed180694a4e
Nikhithasriram <nikhithasriram.552@gmail.com>
2024-11-18 01:13:02 +0530

    changes

--- spice_symphony/src/Components/header.js
+++ spice_symphony/src/Components/header.js
@@ -5,7 +5,7 @@ function Header() {
         <div className='p-5 flex justify-between '>
             <div className='flex'>
                 <div className='py-1'>Logo</div>
-                <div className='px-4 text-2xl'>Spice symphony</div>
+                <div className='px-4 text-2xl'>Spice Symphony</div>
             </div>
             <div>
                 <nav className='list-none flex space-x-6 px-2'>
--- spice_symphony/src/index.js
+++ spice_symphony/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MenuPage from './pages/menu_page';
+// import MenuPage from './pages/menu_page';
+import ReviewsPage from './pages/reviws_page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MenuPage/>
+    <ReviewsPage/>
   </React.StrictMode>
 );
 
